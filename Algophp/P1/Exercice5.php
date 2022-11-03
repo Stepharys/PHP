2023bce9ffbdbb28cd89907013ec01300780b693
Stephane ONADO <stephane.num@gmail.com>
2022-11-03 12:01:14 +0100

    PHP1

--- Algophp/P1/Exercice5.php
+++ Algophp/P1/Exercice5.php
@@ -1,12 +1,14 @@
 <h1>Exercice5</h1>
 
 <p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros. <br>
-Attention, la valeur générée devra être arrondie à 2 décimales.Affichage:
+Attention, la valeur générée devra être arrondie à 2 décimales.
 </p>
 
 <h2>Résultat</h2>
 
 <?php
 
-$pharse = "Montant en francs : "
+intval(mixed $conv, int $$mfranc = 100): int
 
+
+?>
