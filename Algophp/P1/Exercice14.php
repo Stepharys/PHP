685c18729abcdbe3a76d1c7922a83eebe1676f8d
Stephane Onado <stephane.num@gmail.com>
2022-11-14 16:51:52 +0100

    comoto

--- Algophp/P1/Exercice14.php
+++ Algophp/P1/Exercice14.php
@@ -2,4 +2,19 @@
 
 <?php
 
-echo "(si la date courante est le 21/05/2018 et la date de naissance le 17/01/1985 :";
+echo "si la date courante est le 21/05/2018 et la date de naissance le 17/01/1985 :<br>";
+
+$age = 21/05/2018;
+$date = 17/01/1985;
+
+function age($date) { 
+    $age = date('21') - $date; 
+   if (date('md') < date('md', strtotime($date))); { 
+       return $age - 1; 
+   } 
+   return $age; 
+} 
+
+echo age('1980-01-01');
+
+?>
