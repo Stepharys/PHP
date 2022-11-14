90bf227a1d12a330f5df127c43594d32b77dc4b5
Stephane Onado <stephane.num@gmail.com>
2022-11-14 11:13:18 +0100

    comit

--- Algophp/P1/Exercice11.php
+++ Algophp/P1/Exercice11.php
@@ -1,4 +1,15 @@
-<h1>Exercice11</h1>
+<h1>Exercice 11</h1>
 
 <?php
 
+echo "Il y a 4 marque de voitures dans le tableau : <br><br>";
+
+$mvoiture = ["Peugeot","Renault","BMW","Mercedes"];
+
+count($mvoiture);
+
+for ($i = 1; $i <= count($mvoiture); $i++) {
+    echo $mvoiture[$i-1]."<br>";
+}
+
+?>
