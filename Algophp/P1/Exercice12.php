0823017c38cae88e5863c6e32ad496146b522ca1
Stephane Onado <stephane.num@gmail.com>
2022-11-14 14:23:38 +0100

    comit

---
+++ Algophp/P1/Exercice12.php
@@ -0,0 +1,31 @@
+<h1>Exercice 12</h1>
+
+<?php
+
+$langue = ["Mickaël"=>"FRA","Virgile"=>"ESP","Marie-Claire"=>"ENG"];
+
+count($langue);
+
+// for ($i =0; $i <=count($langue); $i++) {
+//     echo $langue[$i-1]."<br>";
+// }
+
+//     if ($langue == "FRA"){
+        
+//     }
+
+    var_dump($langue);
+     for ($i =1 ; $i <=var_dump($langue); $i++ );
+   
+     
+   
+    
+    
+
+
+?>
+
+
+
+
+
