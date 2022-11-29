1a14afa57e54f9f14994656cfc7e49c1d8a93a7d
Stephane.ONADO <stephane.num@gmail.com>
2022-11-29 13:55:51 +0100

    comitt

--- Algophp/P1/Exercice15.php
+++ Algophp/P1/Exercice15.php
@@ -23,15 +23,15 @@ class Personne{
     }
 };
 
-function getAge($date) { 
+function getGilbert($date) { 
    $anniv = date_create($date);
    $timetoday = date_create();
    $age = date_diff($anniv, $timetoday);
-        return $age;
+        return $age->format('%y');
 } 
 $p1= new Personne("DUPONT","Michel","1980-02-19");
 $p2= new Personne("DUCHEMIN","Alice","1985-01-17");
 
-echo $p1->getPrenom()." ".$p1->getName()." a ".getAge($p1->getBirthday())." ans<br>";
-echo $p2->getPrenom()." ".$p2->getName()." a ".getAge($p2->getBirthday())." ans";
+echo $p1->getPrenom()." ".$p1->getName()." a ".getGilbert($p1->getBirthday())." ans<br>";
+echo $p2->getPrenom()." ".$p2->getName()." a ".getGilbert($p2->getBirthday())." ans";
 ?>
