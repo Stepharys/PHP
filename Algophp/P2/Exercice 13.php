3c30a9fe0577c5a9449bcb52e1b1d3f8b0ea2779
stephane.ONADO <stephane.num@gmail.com>
2022-12-01 16:36:07 +0100

    commit

--- Algophp/P2/Exercice 13.php
+++ Algophp/P2/Exercice 13.php
@@ -3,6 +3,7 @@
 <?php
 
 //*Crée la catégorie voiture
+
 class Voiture{
     private $_marque; //La marque de la voiture 
     private $_modele; // Le modèle de la voiture
@@ -56,44 +57,73 @@ class Voiture{
 
     //*Mes fonction
     public function Demarrer(){
-        if($this->_status == "demarrer"){
-            $this->
+        if($this->getStatus()  == false){ //condition****
             echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." démarre<br>";
-        }else{
-            echo "stopper";
-        }
-    }
-
-    public function Accelerer(){
-        if($this->_status == "accelerer"){
-            echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." accélère ".$this->getVitesseActuelle().;
-        }else{
-            echo "stopper";
+        }else{ // si condition non respecté
+            $this->setStatus(true);
+            echo "Le véhicule est deja demarrer<br>";
         }
     }
 
     public function Stopper(){
-        if($this->_status == "stopper"){
-            echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." démarre<br>";
+        if($this->getStatus()  == false){
+            echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." est stopper<br>";
         }else{
-            echo "stopper";
+            $this->setStatus(true);
+            echo "Le véhicule est deja demarrer<br>";
+        }
+    }
+
+    public function accelerer($vitesse){
+        if($this->getStatus() == false){
+            $this->setVitesseActuelle($this->getVitesseActuelle()+$vitesse);
+            echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." accélère de $vitesse km / h<br>";
+        }else{
+            $this->setStatus(true);
+            echo "Veillez demarrer le vehicule<br>";
+        }
+    }
+  
+    public function PoDemarrer(){
+        if($this->getStatus() == false){
+            echo "Pour accélerer, il faut demarrer le vehicule ".$this->getMarque()." ".$this->getModele()." !<br>";
+        }else{
+            $this->setStatus(true);
+            echo "Veillez demarrer le vehicule<br>";
+        }
+    }
+ 
+   
+    public function ralentir($vitesse){
+        if($this->getStatus() == false){
+            echo "Veillez demarrer le vehicule<br>";
+        }else{
+            $this->setStatus(true);
+            $this->setVitesseActuelle($this->getVitesseActuelle()-$vitesse);
+            echo "La vitesse du véhicule ".$this->getMarque()." ".$this->getModele()." est de : $vitesse km / h<br>";
         }
     }
 
 
+
+   
 };
+
 //Crée les information de la voiture
+
 $v1 = New Voiture("Peugeot","408","5");
 $v2 = New voiture("Citroen","C4","3");
-$v1->setStatus("demarrer");
 
-public function Demarrer(){
-    if($this->_status == "demarrer"){
-        $this->
-        echo "Le véhicule ".$this->getMarque()." ".$this->getModele()." démarre<br>";
-    }else{
-        echo "stopper";
-    }
-}
+//Affichage
 
+echo "<br><br>Infos véhicule 1<br>**********<br>Nom et modele du vehicule :".$v1->getMarque()." ".$v1->getModele()."<br>Nombre de portes : ".$v1->getnbPortes()."<br>Le véhicule ".$v1->getMarque()." est demarrer<br>Sa vitesse actuelle est de : ".$v1->getVitesseActuelle()."km / h"; 
+$v1->Demarrer();
+$v1->accelerer(50);
+//echo $v1->getVitesseActuelle() . "<br>";
+$v2->Demarrer();
+$v2->stopper();
+$v1->accelerer(20);
+$v2->PoDemarrer();
+
+echo "<br><br>Infos véhicule 2<br>**********<br>Nom et modele du vehicule :".$v2->getMarque()." ".$v2->getModele()."<br>Nombre de portes : ".$v2->getnbPortes()."<br>Le véhicule ".$v2->getMarque()." est demarrer<br>Sa vitesse actuelle est de : ".$v2->getVitesseActuelle()."km / h"; 
 ?>
