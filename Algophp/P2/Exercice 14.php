f7a96911b277de264135fa439d68e0a650f307b1
Stephane.ONADO <stephane.num@gmail.com>
2022-12-06 10:58:51 +0100

    Herité d'une class

--- Algophp/P2/Exercice 14.php
+++ Algophp/P2/Exercice 14.php
@@ -6,33 +6,48 @@ class Voiture{
     private $_marque; 
     private $_modele; 
 
-    public function __construct($marque, $modele)
+    public function __construct($marque, $modele){
 
         $this->_marque = $marque;
-        $this->modele = $marque;
-
+        $this->_modele = $modele;
+    }
+    
+    //get
     public function getMarque(){
         return $this->_marque;
     }
-    public function  getModele(){
-        return $this->modele;
+    public function getModele(){
+        return $this->_modele;
     }
-
-    echo echo "Le véhicule ".$this->getMarque()." ".$this->getModele().
+    public function getInfo(){
+        echo "Marque et modele du vehicule : ".$this->getMarque()." ".$this->getModele();
+    }
 }
 
-class VoitureElec extends Autonomie{
-
-public function __construct($marque, $modele){
     
-    parent::_construct($marque);
-    $this->_modele = $modele;
-    }
+//extends
+class VoitureElec extends Voiture{
+    
+    private $autonomie;
+
+public function __construct($marque, $modele, $autonomie){
+    
+    parent::__construct($marque, $modele);
+    $this->_autonomie = $autonomie;
+}
+public function getAutonomie(){
+    return $this->_autonomie;
+}
+public function getInfoElec(){
+echo "Marque, modele du vehicule et son autonomie : ".$this->getMarque()." ".$this->getModele()." ".$this->getAutonomie();
 } 
-echo Autonomie::combien();
+}
+//information/affichage 
 
 $v1 = new Voiture("Peugeot","408");
 $ve1 = new VoitureElec("BMW","I3",100);
 
 echo $v1->getInfo()."<br>";
-echo $ve1->getInfo()."<br>";
+echo $ve1->getInfoElec()."<br>";
+
+?>
