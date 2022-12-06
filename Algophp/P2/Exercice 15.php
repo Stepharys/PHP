78357730699a46569d11176f19701ba1419abbfa
Stephane.ONADO <stephane.num@gmail.com>
2022-12-06 09:35:19 +0100

    Validation email

--- Algophp/P2/Exercice 15.php
+++ Algophp/P2/Exercice 15.php
@@ -3,14 +3,10 @@
 <?php
 
 $email_a = 'elan@elan-formation.fr';
-$email_b = 'contact@elan';
 
 if (filter_var($email_a, FILTER_VALIDATE_EMAIL)) {
-    echo "L'adresse email '$email_a' est considérée comme valide.<br>";
-}
-if (filter_var($email_b, FILTER_VALIDATE_EMAIL)) {
-    echo "L'adresse email '$email_b' est considérée comme valide.";
+    echo "L'adresse email '$email_a' est considérée comme valide.";
 } else {
-    echo "L'adresse email '$email_b' est considérée comme invalide.";
+    echo "L'adresse email '$email_a' est considérée comme invalide.";
 }
 ?>
