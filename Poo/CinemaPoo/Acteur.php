03e46634f0211c06d353ef3c1035f4d08eab457a
Stepharys <103320026+Stepharys@users.noreply.github.com>
2022-12-12 09:42:14 +0100

    Création construct

--- Poo/CinemaPoo/Acteur.php
+++ Poo/CinemaPoo/Acteur.php
@@ -1,5 +1,31 @@
 <?php
+class Actor extends Person
+{
+    private $_castings;
 
-class Acteur{
-    private string $_
+    public function __construct($lastname, $firstname, $sexe, $dateofbirth)
+    {
+        parent::__construct($lastname, $firstname, $sexe, $dateofbirth);
+        $this->_castings=[];
+    }
+    function getFilmographie()
+    {
+        foreach ($this->_castings as $film)
+        {
+            return $film->getMovie()->getTitle()."<br>";
+        }
+    }
+
+    public function addcasting($newcasting)
+    {
+        $this->_castings [] = $newcasting;
+    }
+
+    public function getCasting()
+    {
+        foreach ($this->_castings as $casting) 
+        {
+            echo $casting->getActor() ." a joué ". $casting->getRole()."<br>";
+        }
+}
 }
