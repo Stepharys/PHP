e1b070599bdb737ff33a8fc99f6140551c702ae1
stephane.onado <stephane.num@gmail.com>
2022-12-12 10:10:43 +0100

    add role

--- Poo/CinemaPoo/index.php
+++ Poo/CinemaPoo/index.php
@@ -4,12 +4,16 @@ spl_autoload_register(function($class_name){
     require_once $class_name . '.php';
 });
 
-$b1 = new personne("Michael","Keaton","man",'05/09/1951');
-$b2 = new personne("Val","Kilmer","man",'31/12/1959');
-$b3 = new personne("George","Clooney","man",'06/05/1961');
+$b1 = new acteur("Michael","Keaton","man",'05/09/1951');
+$b2 = new acteur("Val","Kilmer","man",'31/12/1959');
+$b3 = new acteur("George","Clooney","man",'06/05/1961');
 
-$c1 = new personne("Harrison","Ford","man",'13/06/1942');
-$c2 = new personne("Mark","Hamill","man",'25/09/1951');
+$c1 = new acteur("Harrison","Ford","man",'13/06/1942');
+$c2 = new acteur("Mark","Hamill","man",'25/09/1951');
+
+$r1 = new role("Batman")
+$r2 = new role("Han Solo")
+$r3 = new role("Luke skywalker")
 
 $action = new genreCiné("action");
 $aventure = new genreCiné("aventure");
