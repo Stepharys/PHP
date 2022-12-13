233c13acdbf09f3ac9581721e80e70cf62d43b22
stephane.onado <stephane.num@gmail.com>
2022-12-13 16:17:56 +0100

    add new film new genre Realisateur

--- Poo/CinemaPoo/index.php
+++ Poo/CinemaPoo/index.php
@@ -4,23 +4,29 @@ spl_autoload_register(function($class_name){
     require_once $class_name . '.php';
 });
 
-$aBa1 = new acteur("Michael","Keaton","man",'05/09/1951');
-$aBa2 = new acteur("Val","Kilmer","man",'31/12/1959');
-$aBa3 = new acteur("George","Clooney","man",'06/05/1961');
+//$aBa1 = new acteur("Michael","Keaton","man",'05/09/1951');
+//$aBa2 = new acteur("Val","Kilmer","man",'31/12/1959');
+//$aBa3 = new acteur("George","Clooney","man",'06/05/1961');
 
-$aSw1 = new acteur("Harrison","Ford","man",'13/06/1942');
-$aSw2 = new acteur("Mark","Hamill","man",'25/09/1951');
+//$aSw1 = new acteur("Harrison","Ford","man",'13/06/1942');
+//$aSw2 = new acteur("Mark","Hamill","man",'25/09/1951');
 
 $rBa = new Realisateur("Tim","Burton","man",'25/08/1958');
 
-$rSw = new Realisateur("Bob","Rob","man",'15/12/1965');
+$action = new Genre("action");
 
-$ro1 = new Role("Batman");
-$ro2 = new Role("Han Solo");
-$ro3 = new Role("Luke skywalker");
+//$rSw = new Realisateur("Bob","Rob","man",'15/12/1965');
 
-$action = new genreCin("action");
-$aventure = new genreCin("aventure");
-$Sf = new genreCin("science-fiction");
+//$ro1 = new Role("Batman");
+//$ro2 = new Role("Han Solo");
+//$ro3 = new Role("Luke skywalker");
 
+//$aventure = new Genre("aventure");
+//$Sf = new Genre("science-fiction");
+
+//$aBa1->getCasting();
+
+//echo $rBa->getLastName();
+//echo $action;
+$fi1 = new Film("Batman","05/04/2001",120,$action,$rBa,"lol");
 ?>
