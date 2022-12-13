d1a67a1cf333698853f5ee11f1e5894dfd3b9b37
stephane.onado <stephane.num@gmail.com>
2022-12-13 10:26:41 +0100

    construct et get

---
+++ Poo/CinemaPoo/Realisateur.php
@@ -0,0 +1,28 @@
+<?php
+class Realisateur extends Human
+{
+    $private string $_film;
+
+    public function __construct($lastname, $firstname, $sexe, $birthday)
+    {
+        parent::__construct($lastname, $firstname, $sexe, $birthday);
+        $this->_castings=[];
+    }
+
+    //get
+    function getFilmographie(){
+        foreach ($this->_castings as $film){
+        return $film->getMovie()->getTitle()."<br>";
+        }
+    }
+
+    public function addcasting($newcasting){
+        $this->_castings [] = $newcasting;
+    }
+
+    public function getCasting(){
+        foreach ($this->_castings as $casting){
+        echo "L'acteur".$casting->getActeur() ." a joué ". $casting->getRole()."<br>";
+        }
+}
+}
