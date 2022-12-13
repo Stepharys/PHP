63c30fcff9992fc24b1aa9645318621501aac2ce
stephane.onado <stephane.num@gmail.com>
2022-12-13 10:25:50 +0100

    changé et ajout d'information acteur et realisateur

--- Poo/CinemaPoo/index.php
+++ Poo/CinemaPoo/index.php
@@ -4,19 +4,23 @@ spl_autoload_register(function($class_name){
     require_once $class_name . '.php';
 });
 
-$ba1 = new personnage("Michael","Keaton","man",'05/09/1951');
-$ba2 = new personnage("Val","Kilmer","man",'31/12/1959');
-$ba3 = new personnage("George","Clooney","man",'06/05/1961');
+$aBa1 = new acteur("Michael","Keaton","man",'05/09/1951');
+$aBa2 = new acteur("Val","Kilmer","man",'31/12/1959');
+$aBa3 = new acteur("George","Clooney","man",'06/05/1961');
 
-$sw1 = new personnage("Harrison","Ford","man",'13/06/1942');
-$sw2 = new personnage("Mark","Hamill","man",'25/09/1951');
+$aSw1 = new acteur("Harrison","Ford","man",'13/06/1942');
+$aSw2 = new acteur("Mark","Hamill","man",'25/09/1951');
+
+$rBa = new Realisateur("Tim","Burton","man",'25/08/1958');
+
+$rSw = new Realisateur("Bob","Rob","man",'15/12/1965');
 
 $ro1 = new Role("Batman");
 $ro2 = new Role("Han Solo");
 $ro3 = new Role("Luke skywalker");
 
-$action = new genreCiné("action");
-$aventure = new genreCiné("aventure");
-$Sf = new genreCiné("science-fiction");
+$action = new genreCin("action");
+$aventure = new genreCin("aventure");
+$Sf = new genreCin("science-fiction");
 
 ?>
