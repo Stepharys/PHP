c46a75fca9cd5d17d84753c706e7114f258c81f6
stephane.onado <stephane.num@gmail.com>
2023-01-09 01:37:17 +0100

    public funcction

--- Poo/FootballPoo/Club.php
+++ Poo/FootballPoo/Club.php
@@ -4,6 +4,41 @@ class Club{
 
 Private $_nom;
 
+public function __construct($title, $date, $time, Genre $genre, Realisateur $realisateur, $synopsis){
+
+    $this->_title = $title;
+    $this->_date = $date;
+    $this->_time = $time;
+    $this->_genre = $genre;
+    $this->_realisateur = $realisateur;
+    $this->_synopsis = $synopsis;
+    $this->_casting = [];
+    $this->_realisateur->Tabfilm($this);
+    $this->_genre->Tabfilm($this);
+}
+
+//get
+public function getTitle(){
+    return $this->_title;
+}
+public function getDate(){
+    return $this->_nom;
+}
+public function getTime(){
+    return $this->_time;
+}
+public function getRealisateur(){
+    return $this->_réalistateur;
+}
+public function getsynopsis(){
+    return $this->_synopsis;
+}
+public function getGenre(){
+    return $this->_genre;
+}
+public function getCasting(){
+    return $this->_casting;
+}
 
 
 }
--- Poo/FootballPoo/Nationalité.php
+++ Poo/FootballPoo/Nationalité.php
@@ -1,9 +1,31 @@
 <?php
 
 class Nationalité{
-
-Private $_nom;
-
-
-
-}
+    
+        protected $_name;
+        protected $_players;
+            
+        public function __construct(string $name){
+            $this->_name = $name;
+            $this->_players = [];
+        }
+                
+        //GET
+        public function getName(){
+            return $this->_name;
+        }
+    
+            
+        //SET
+        public function setName($name){
+            $this->_name = $name;
+        }
+        public function setNationality($nationality){
+            $this->_nationality = $nationality;
+        } 
+    
+        public function addPlayer($player){
+            array_push($this->_players,$player);
+        }
+    }
+    ?>
